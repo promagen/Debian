9f2ca9dd23d5f710ca7441c81d5c5c50dde1f612
tom <tom@sapletta.com>
2020-05-24 19:58:57 +0200

    update

--- update.sh
+++ update.sh
@@ -3,6 +3,6 @@ git pull origin master
 cd node && git pull origin master && cd ..
 sh node/update.sh
 cd promagen && git pull origin master && cd ..
-sh promagen/update.sh
+#sh promagen/update.sh
 #cd php && git pull origin master && cd ..
 #sh php/update.sh
