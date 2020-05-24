a710b45530d3411fc68ed13826abd073ee5e0122
tom <tom@sapletta.com>
2020-05-24 16:27:28 +0200

    update

--- promagen.sh
+++ promagen.sh
@@ -1,4 +1,17 @@
+#apt update
+#command -v git >/dev/null 2>&1 || { echo >&2 "I require git but it's not installed.  Aborting."; exit 1; }
+#command -v git || { echo >&2 "I require git but it's not installed.  I will try install it first."; apt install git && git --version; }
+if ! [ -x "$(command -v git)" ]; then
+  echo 'git is not installed. I will try install it first'
+  apt install git
+  git --version
+fi
+
+if ! [ -x "$(command -v git)" ]; then
+  echo 'Error: git is not installed.' >&2
+  exit 1
+fi
+
 git clone https://github.com/apicra/promagen-debian.git promagen-debian
 cd promagen-debian
 sh download.sh
-
